9eaf6699dfd157854794e6e38a0a9cde9a1a04a0
jrojer <cibid@yandex.ru>
2020-05-12 20:32:13 +0300

    moved db

--- server/project/config.py
+++ server/project/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = '9OLWxND4o83j4K4iuopO'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, '../app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #CORS_HEADERS = 'Access-Control-Allow-Origin'
